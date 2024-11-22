27aef8f5e7c662d16aa5e33f7869e8d3e6cd34e9
Govind <govindup63@gmail.com>
2024-11-22 22:53:41 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/upload', (req, res) => {
+
+})
+
+
+async function main() {
+
+  app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`)
+  })
+}
